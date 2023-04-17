ad53411e0c47e7832746348f07c6168f58fc51a0
Narwhal Kid <55326739+NarwhalKid@users.noreply.github.com>
2023-04-17 14:59:14 -0400

    Update games.js

--- games.js
+++ games.js
@@ -1,2 +1,2 @@
 // set list for all games that can be changed through github
-var games = [["slither.io", "https://1-slither.glitch.me/"], ["1v1.lol", "https://1-1v1.glitch.me/"]]
+var games = [["slither.io", "https://1-slither.glitch.me/", "1"], ["1v1.lol", "https://1-1v1.glitch.me/", "0"]]
