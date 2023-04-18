e8b2d30a3a0893223c465ee933e214c7a71bebcc
Narwhal Kid <55326739+NarwhalKid@users.noreply.github.com>
2023-04-18 07:32:45 -0400

    Update games.js

--- games.js
+++ games.js
@@ -1,2 +1,2 @@
 // set list for all games that can be changed through github
-var games = [["slither.io", "https://1-slither.glitch.me/", "1"], ["1v1.lol", "https://1-1v1.glitch.me/", "0"], ["diep.io", "https://1-diep.glitch.me/", "0"]]
+var games = [["slither.io", "https://1-slither.glitch.me/", "1"], ["1v1.lol", "https://1-1v1.glitch.me/", "0"], ["diep.io", "https://1-diep.glitch.me/", "0"], ["basket random", "https://narwhalkidgames.narwhalkid.repl.co/games/basketrandom/", "1"], ["boxing random", "https://narwhalkidgames.narwhalkid.repl.co/games/boxingrandom/", "1"]]
